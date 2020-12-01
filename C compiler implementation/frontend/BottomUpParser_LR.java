0026a64ccccf4906d928331eca0f06a3552935de
81258 <812583792@qq.com>
2020-12-01 15:20:13 +0800

    CODE GENERATE

--- C compiler implementation/frontend/BottomUpParser_LR.java
+++ C compiler implementation/frontend/BottomUpParser_LR.java
@@ -23,6 +23,9 @@ public class BottomUpParser_LR {
         Intepretor intepretor = Intepretor.getIntepretor();
         intepretor.Execute(treeBuilder.getCodeTreeRoot());
 
+        System.out.println("hello world!");
+
+
     }
 
 }
